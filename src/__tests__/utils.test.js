2bc776ea8bfbaa66a3d41d93fe0f39261b6218a5
ashleymararo <ashleymararo222@gmail.com>
2025-07-29 21:14:35 +0300

    finished lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true for a palindrome like 'racecar'", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it("returns false for a non-palindrome like 'car'", () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  it("is case-insensitive: 'Mom' should return true", () => {
+    expect(isPalindrome("Mom")).toBe(true);
+  });
+
+  it("returns false for an empty string", () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  it("throws an error for non-alphabetic characters", () => {
+    expect(() => isPalindrome("ra.cecar")).toThrow("Word must contain only alphabetic characters");
+  });
+
+  it("throws an error if input is not a string", () => {
+    expect(() => isPalindrome(123)).toThrow("Input must be a string");
+    expect(() => isPalindrome(null)).toThrow("Input must be a string");
+  });
+});
